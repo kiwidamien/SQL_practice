90279587806c252e6397669781387a4d5ed56831
kiwidamien <djmartin@ucdavis.edu>
2019-01-22 14:25:36 -0800

    terminated install scripts

--- install_problems.sh
+++ install_problems.sh
@@ -3,4 +3,4 @@
 # Goes to each subdirectory, and runs 'psql -f install.sh', so that directories install
 # script can create the tables in the database
 #
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && psql -f install.sql"
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && psql -f install.sql" \;
--- uninstall_problems.sh
+++ uninstall_problems.sh
@@ -3,4 +3,4 @@
 # Goes to each subdirectory, and runs 'psql -f clean.sh', which removes the database installed 
 # for that directory.
 #
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && psql -f clean.sql"
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && psql -f clean.sql" \;
